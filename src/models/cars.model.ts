9e11a2d20469ca8c1eb5da3eea10ad729c5fbecb
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-09-16 10:22:03 -0300

    feat: create car model

---
+++ src/models/cars.model.ts
@@ -0,0 +1,18 @@
+import { model as mongooseCreateMode, Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar'; 
+import MongoModel from './mongo.model';
+
+const carsMongooseSchema = new Schema<ICar>({
+  model: String,
+  year: Number,
+  color: String,
+  buyValue: Number,
+  seatsQty: Number,
+  doorsQty: Number,
+});
+
+export default class Cars extends MongoModel<ICar> {
+  constructor(model = mongooseCreateMode('Cars', carsMongooseSchema)) {
+    super(model);
+  }
+}
